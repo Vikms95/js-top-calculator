6c7f4e1222dc617a7bfb4bd1d4020322392acc9b
Vikms95 <vms1995@hotmail.com>
2022-01-09 05:37:30 +0100

    Add listeners to num and clear button

--- notes.md
+++ notes.md
@@ -1,2 +1,9 @@
-4. Get a NodeList reference of all the buttons in the document and turn it into a list.Add eventListeners to buttons and modify display div textContent with the value of the button pressed 
-//TODO figure out best way to store display value
+4. -Get a NodeList reference of all the .num-buttons in the document and turn it into a list.
+    -Create a function declared at the end of JS that adds all required eventListeners with sub functions for each type of button
+    -Add different type of eventListeners depending on the button's behaviour  
+        -xAdd eventListeners to .num-buttons(brackets and decimal included) and modify display div textContent with the value of the button pressed 
+        -xAdd eventListener to .clear-button
+        -Add eventListeners to .op-button (if operator is clicked, store number)
+        -Add eventListeners to .equals-button
+        
+    //TODO figure out best way to store display value(narrow it down to 2 numbers first)
--- script.js
+++ script.js
@@ -18,13 +18,51 @@ function operate(operator,operand1,operand2) {
     return operator(operand1,operand2);
 }
 
-let buttonNodeListReference = document.querySelectorAll("button");
-let displayReference = document.querySelector(".display")
-let displayValue = 0;
-for (let i = 0; i < buttonNodeListReference.length; i++) {
-    buttonNodeListReference[i].addEventListener("click", (e) =>{
-    displayReference.textContent += " " + e.target.textContent;
-    displayValue = displayReference.textContent;
-    console.log(displayValue);
-    });
+function isFirstOperation(displayValue){
+    return displayValue == 0 ? true : false;
 }
+
+function addListenerNumButton (buttonReferenceNumNodeList,displayReference){
+    for (let i = 0; i < buttonReferenceNumNodeList.length; i++) {
+        buttonReferenceNumNodeList[i].addEventListener("click", (e) =>{
+        displayReference.textContent += e.target.textContent;
+        })
+    };
+}
+
+function addListenerClearButton(buttonReferenceClear,displayReference){
+        buttonReferenceClear.addEventListener("click", () => {
+        displayReference.textContent = "";
+        displayValue = 0;
+    })
+}
+
+// function addListenerOpButton(buttonReferenceOpNodeList,displayReference){
+        
+//     for (let i = 0; i < buttonReferenceOpNodeList.length; i++) {
+//         buttonReferenceOpNodeList[i].addEventListener("click", () => {
+//             if(isFirstOperation()){
+//                 let displayOperand1 =
+//             }
+            
+//             let displayOperand2
+//             displayValue = displayReference.textContent;
+//         });
+//     };
+// };
+
+function pageBoot(){
+
+    const buttonReferenceNum = document.querySelectorAll(".num-button");
+    const buttonReferenceOp = document.querySelectorAll(".op-button");
+    const buttonReferenceClear = document.querySelector(".clear-button");
+    const displayReference = document.querySelector(".display");
+    let displayValue = 0;
+
+    addListenerNumButton(buttonReferenceNum,displayReference);
+    addListenerClearButton(buttonReferenceClear,displayReference);
+    addListenerOpButton(buttonReferenceOp,displayReference)
+    console.log(displayValue);
+};
+
+pageBoot();
